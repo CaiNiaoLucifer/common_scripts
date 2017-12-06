a14b70ed8b5de8f1ce19852ff701f94588f24071
duyongwei <duyongwei@xiaomi.com>
2017-12-06 17:25:08 +0800

    add sublime_command_line_on_mac.sh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # common_scripts
+
+## macos下面安装Sublime Text 3的命令行工具
+sublime_command_line_on_mac.sh
---
+++ sublime_command_line_on_mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "installing command line of Sublime Text 3 for macos"
+
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
